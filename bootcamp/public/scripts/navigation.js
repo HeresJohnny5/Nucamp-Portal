9c3214ca333efd88d2d215b0055a78bb8cb19ce2
HeresJohnny5 <ericksonjohn01@hotmail.com>
2021-01-14 23:30:50 -0500

    Change HTML structure slightly, adding a button to show/hide the Sidebar.

--- bootcamp/public/scripts/navigation.js
+++ bootcamp/public/scripts/navigation.js
@@ -1,5 +1,18 @@
 $(function () {
+  // Sidebar Navigation Submenu Accordions
   $("#accordion").on("hide.bs.collapse show.bs.collapse", (e) => {
     $(e.target).prev().find("i:last-child").toggleClass("fa-minus fa-plus");
+  });
+
+  // Sidebar Show/Hide Functionality
+  $(".sidebarBtn").on("click", () => {
+    $("nav").toggleClass("inactive-sidebar");
+    $("main").toggleClass("portion-main");
+
+    if ($("nav").hasClass("inactive-sidebar")) {
+      $(".sidebarBtn").html("Show Sidebar");
+    } else {
+      $(".sidebarBtn").html("Hide Sidebar");
+    }
   });
 });
